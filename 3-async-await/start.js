9e6ce5bd5ac63cd4e8d252bc0ffcfcbea43d4039
Apipol Sukgler <golf.apipol@gmail.com>
2020-03-31 21:37:47 +0700

    [Edited]: add promise example

--- 3-async-await/start.js
+++ 3-async-await/start.js
@@ -6,20 +6,34 @@
     // simulateAsyncAPI('B', 500)
     // simulateAsyncAPI('C', 100)
 
-    const simulateAsyncAPICallback = (text, timeout, callback) => {
-        setTimeout(() => {
-            console.log(text)
-            if (callback) {
-                callback()
-            }
-        }, timeout)
+    // const simulateAsyncAPICallback = (text, timeout, callback) => {
+    //     setTimeout(() => {
+    //         console.log(text)
+    //         if (callback) {
+    //             callback()
+    //         }
+    //     }, timeout)
+    // }
+
+    // simulateAsyncAPICallback('A', 1000, () => {
+    //     simulateAsyncAPICallback('B', 500, () => {
+    //         simulateAsyncAPICallback('C', 100, () => {
+    //             console.log('Callback')
+    //         })
+    //     })
+    // })
+
+    const simulateAsyncAPIPromise = (text, timeout) => {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                console.log(text)
+                resolve()
+            }, timeout)
+        })
     }
 
-    simulateAsyncAPICallback('A', 1000, () => {
-        simulateAsyncAPICallback('B', 500, () => {
-            simulateAsyncAPICallback('C', 100, () => {
-                console.log('Callback')
-            })
-        })
-    })
+    simulateAsyncAPIPromise('A', 1000)
+        .then(() => simulateAsyncAPIPromise('B', 500))
+        .then(() => simulateAsyncAPIPromise('C', 100))
+    
 })()
