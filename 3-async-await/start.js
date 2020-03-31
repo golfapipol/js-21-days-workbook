21545f47140163853ef8db3c4863108059252dde
Apipol Sukgler <golf.apipol@gmail.com>
2020-03-31 21:34:06 +0700

    [Edited]: add callback example

--- 3-async-await/start.js
+++ 3-async-await/start.js
@@ -1,9 +1,23 @@
 (() => {
-    const simulateAsyncAPI = (text, timeout) => {
-        setTimeout(() => console.log(text), timeout)
+    // const simulateAsyncAPI = (text, timeout) => {
+    //     setTimeout(() => console.log(text), timeout)
+    // }
+    // simulateAsyncAPI('A', 1000)
+    // simulateAsyncAPI('B', 500)
+    // simulateAsyncAPI('C', 100)
+
+    const simulateAsyncAPICallback = (text, timeout, callback) => {
+        setTimeout(() => {
+            console.log(text)
+            callback()
+        }, timeout)
     }
 
-    simulateAsyncAPI('A', 1000)
-    simulateAsyncAPI('B', 500)
-    simulateAsyncAPI('C', 100)
+    simulateAsyncAPICallback('A', 1000, () => {
+        simulateAsyncAPICallback('B', 500, () => {
+            simulateAsyncAPICallback('C', 100, () => {
+                console.log('Callback')
+            })
+        })
+    })
 })()
