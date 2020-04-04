7233e28168e790a69b50a20e0afb5d265b2741d0
Apipol Sukgler <golf.apipol@gmail.com>
2020-04-04 11:15:06 +0700

    [Edited]: line weight up to speed of mouse

--- 5-water-painting/start.js
+++ 5-water-painting/start.js
@@ -7,13 +7,20 @@
 
     let previousPoint = {x:0, y:0}
 
+    const getDistance = (previousPoint, currentPoint) => {
+        return Math.sqrt((previousPoint.x - currentPoint.x) ** 2 + (previousPoint.y - currentPoint.y) ** 2)
+    }
+
     const onMouseMove = ({pageX, pageY}) => {
         const currentPoint = { x:pageX, y:pageY}
         context.beginPath()
         context.lineCap = 'round'
         context.lineJoin = 'round'
         context.lineWidth = 40
-        context.strokeStyle = `rgba(222,10,109,0.8)`
+
+        const distance = getDistance(previousPoint,currentPoint)
+        const opacity = Math.min(0.5, 1 / distance)
+        context.strokeStyle = `rgba(222,10,109,${opacity})`
 
         context.moveTo(previousPoint.x,previousPoint.y)
         context.lineTo(currentPoint.x, currentPoint.y)
