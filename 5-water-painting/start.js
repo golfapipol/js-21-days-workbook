3ee3371595c28f2eec714ae0ac1ecb3d8919e2ad
Apipol Sukgler <golf.apipol@gmail.com>
2020-04-04 11:35:15 +0700

    [Edited]: refactor to pure function

--- 5-water-painting/start.js
+++ 5-water-painting/start.js
@@ -1,24 +1,17 @@
 (()=> {
-    const canvas = document.getElementById("painting")
-    canvas.width = window.innerWidth
-    canvas.height = window.innerHeight
-
-    const context = canvas.getContext('2d')
-
-    let previousPoint = {x:0, y:0}
-
+    
     const getDistance = (previousPoint, currentPoint) => {
         return Math.sqrt((previousPoint.x - currentPoint.x) ** 2 + (previousPoint.y - currentPoint.y) ** 2)
     }
 
-    const onMouseMove = ({pageX, pageY}) => {
+    const onMouseMove = (context, previousPoint) => ({pageX, pageY}) => {
         const currentPoint = { x:pageX, y:pageY}
         context.beginPath()
         context.lineCap = 'round'
         context.lineJoin = 'round'
-        context.lineWidth = 40
 
         const distance = getDistance(previousPoint,currentPoint)
+        context.lineWidth = Math.random() / distance *40
         const opacity = Math.min(0.5, 1 / distance)
         context.strokeStyle = `rgba(222,10,109,${opacity})`
 
@@ -29,14 +22,21 @@
         previousPoint = currentPoint
     }
 
-    const onMouseEnter = ({pageX, pageY}) => {
+    const onMouseEnter = (previousPoint) => ({pageX, pageY}) => {
         previousPoint.x = pageX
         previousPoint.y = pageY
     }
 
     const run = () => {
-        canvas.addEventListener('mouseenter', onMouseEnter)
-        canvas.addEventListener('mousemove', onMouseMove)
+        const canvas = document.getElementById("painting")
+        canvas.width = window.innerWidth
+        canvas.height = window.innerHeight
+
+        const context = canvas.getContext('2d')
+
+        let previousPoint = {x:0, y:0}    
+        canvas.addEventListener('mouseenter', onMouseEnter(previousPoint))
+        canvas.addEventListener('mousemove', onMouseMove(context, previousPoint))
     }
     run()
 })()
