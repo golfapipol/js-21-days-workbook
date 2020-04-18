6f3e4e8cfcb37182640e55f243ed1e21ac25abee
Apipol Sukgler <golf.apipol@gmail.com>
2020-04-18 15:50:32 +0700

    [Edited]: complete parallax and add mario to screen

--- 9-parallax/start.js
+++ 9-parallax/start.js
@@ -1,3 +1,18 @@
 (() => {
-    
+    let originalScrollY = window.scrollY
+    const onScroll = () => {
+        const moonElement = document.querySelector('.moon')
+        const wishElement = document.querySelector('.wish')
+        const marioElement = document.querySelector('.mario')
+        
+        moonElement.style.transform = `translate(${window.scrollY * 0.7}%, ${window.scrollY * -0.7}%)`
+        wishElement.style.transform = `translateY(${window.scrollY * -1.2}%)`
+        marioElement.style.transform = `translateX(${window.scrollY * 1.2}%)`
+    }
+
+    const run = () => {
+        document.addEventListener('scroll', onScroll)
+    }
+
+    run();
 })()
