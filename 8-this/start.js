0fc4430b750a0ed9eabba3ab49362da2305b1684
Apipol Sukgler <golf.apipol@gmail.com>
2020-04-18 15:13:01 +0700

    [Edited]: add sample of this and call,apply,bind

--- 8-this/start.js
+++ 8-this/start.js
@@ -1,19 +1,48 @@
 (() => {
     //lexical scope, dynamic scope
-    function printName() {
-        console.log(this)
-        console.log(this.name)
-    }
+    // function printName() {
+    //     console.log(this)
+    //     console.log(this.name)
+    // }
     // printName()
 
-    //invoker object
-    const person = { name: 'John', printName}
-    const person2 = { name: 'Jane', printName}
+    // 2.1 invoker object
+    // const person = { name: 'John', printName}
+    // const person2 = { name: 'Jane', printName}
     // person.printName()
     // person2.printName()
-    
-    // global
-    name = 'G'
-    printName()
+
+    // 2.2 global
+    // name = 'G'
+    // printName()
+
+    // 2.3 constructor function
+    // function Person(name) {
+    //     this.name = name
+    //     this.printName = printName
+    // }
+
+    // const person3 = new Person('Joker')
+    // person3.printName()
+
+    // 3. call(), apply(), bind()
+    function printName(nationality, city) {
+        console.log(this)
+        console.log(`my name is ${this.name}, nationality is ${nationality} and city ${city}`)
+    }
+
+    function Person(name, nationality, city) {
+        this.name = name
+        this.nationality = nationality
+        this.city = city
+        
+        printName(this.nationality, this.city)
+        printName.call(this, this.nationality, this.city)
+        printName.apply(this, [this.nationality, this.city])
+
+        const printNamePerson = printName.bind(this)
+        printNamePerson()
+    }
+    const person4 = new Person('Joker','English', 'Gotham')
 
 })();
