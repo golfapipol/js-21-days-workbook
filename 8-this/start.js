7508a2915e75d2285113f08fab8f06a6e49ab17c
Apipol Sukgler <golf.apipol@gmail.com>
2020-04-18 14:52:05 +0700

    [Edited]: add sample global object access

--- 8-this/start.js
+++ 8-this/start.js
@@ -2,14 +2,18 @@
     //lexical scope, dynamic scope
     function printName() {
         console.log(this)
+        console.log(this.name)
     }
-    printName()
+    // printName()
 
     //invoker object
     const person = { name: 'John', printName}
     const person2 = { name: 'Jane', printName}
-    person.printName()
-    person2.printName()
+    // person.printName()
+    // person2.printName()
     
+    // global
+    name = 'G'
+    printName()
 
 })();
