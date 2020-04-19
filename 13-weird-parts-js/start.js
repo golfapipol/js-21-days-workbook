1b8fdceacaba6200eb2b2cdb0663cf0cd30ab72d
Apipol Sukgler <golf.apipol@gmail.com>
2020-04-19 13:36:49 +0700

    [Edited]: add type coercion/auto cast type

--- 13-weird-parts-js/start.js
+++ 13-weird-parts-js/start.js
@@ -1,15 +1,25 @@
 (() => {
-    // 1. NaN
-    if (null === null) {
-        console.log('null Equal null')
-    }
-    if (NaN !== NaN) {
-        console.log('Nan cannot equal self because NaN/NaN != 1')
-    }
-    const result = 1 / 'hello'
-    // cannot use NaN === NaN in if use Number.isNaN
-    if (Number.isNaN(result)) {
-        console.log('Result is NaN')
-    }
+    // // 1. NaN
+    // if (null === null) {
+    //     console.log('null Equal null')
+    // }
+    // if (NaN !== NaN) {
+    //     console.log('Nan cannot equal self because NaN/NaN != 1')
+    // }
+    // const result = 1 / 'hello'
+    // // cannot use NaN === NaN in if use Number.isNaN
+    // if (Number.isNaN(result)) {
+    //     console.log('Result is NaN')
+    // }
 
+    // 2. Type Coercion
+    // 1 < 2 < 3
+    // true < 3
+    // true
+    //
+    // 3 > 2 > 1
+    // true > 1 
+    // false
+    
+    
 })()
